651eee21d80871ae2cc8c74d2029656a8024eb18
masterUNG <phrombutr@gmail.com>
2019-12-12 16:08:39 +0700

    Finish 12Dec

--- lib/firebase1.dart
+++ lib/firebase1.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:like_button/like_button.dart';
 import 'package:share/share.dart';
 
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -14,6 +13,12 @@ class _HomeScreenState extends State<HomeScreen> {
   int counter = 0;
 
   var msg;
+
+  @override
+  void initState() {
+    super.initState();
+    readAllData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,10 +55,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       setState(() {
                         counter = 0;
                       });
-                    }
-                    ),
-                counter != 0? 
-                Positioned(
+                    }),
+                counter != 0
+                    ? Positioned(
                         right: 11,
                         top: 11,
                         child: Container(
@@ -81,122 +85,136 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        
-
-
         body: StreamBuilder(
             stream: Firestore.instance.collection('post').snapshots(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
                 const Text('Loading');
               } else {
-                return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot mypost = snapshot.data.documents[index];
+                return snapshot.hasData
+                    ? showListView(snapshot)
+                    : Center(
+                        child: CircularProgressIndicator(),
+                      );
+              } //##############
+            }));
+  }
 
-                    return SingleChildScrollView(
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(
-                                left: 10.0, right: 15.0, bottom: 8.0, top: 15),
-                            child: Row(
-                              children: <Widget>[
-                                Row(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    makeFeed(
-                                      userName: 'KickIt App Admin',
-                                      userImage: 'assets/Ton.jpg',
-                                      feedTime: '${mypost['time']}',
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(
-                                left: 10.0, right: 10.0, ),
-                                child: Column(
-                                  children: <Widget>[
-                                    Image.network('${mypost['image']}',),
-                                  ],
-                                ),
-                          ),
-                          
+  Future<void> readAllData() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('post');
+    await collectionReference.snapshots().listen((dataSnapshot) {
+      for (var object in dataSnapshot.documents) {
+        
+      }
+    });
+  }
 
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Text(
-                                    '${mypost['title']}',
-                                    overflow: TextOverflow.fade,
-                                    maxLines: 3,
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black),
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: <Widget>[
-                                    IconButton(icon: Icon(Icons.share),
-                                    onPressed: () => Share.share("https://web.facebook.com/ekachai.sanharn"),
-                                    iconSize: 20,
-                                    
-                                    ),
-                                    likeButton(),
-                                    ],
-                                ),
-                            
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Expanded(
-                                    child: Text(
-                                  '${mypost['subtitle']}',
-                                  overflow: TextOverflow.fade,
-                                  maxLines: 99,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal,
-                                      color: Colors.black),
-                                )),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(height: 15.0,color: Colors.grey.withOpacity(0.4),),
-                          
-                          SizedBox(
-                            height: 5,
-                          ),
-                        ],
+  Widget showListView(var snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.data.documents.length,
+      itemBuilder: (context, index) {
+        DocumentSnapshot mypost = snapshot.data.documents[index];
+
+        return SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 10.0, right: 15.0, bottom: 8.0, top: 15),
+                child: Row(
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        makeFeed(
+                          userName: 'KickIt App Admin',
+                          userImage: 'assets/Ton.jpg',
+                          feedTime: '${mypost['time']}',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: 10.0,
+                  right: 10.0,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                      '${mypost['image']}',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        '${mypost['title']}',
+                        overflow: TextOverflow.fade,
+                        maxLines: 3,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       ),
-                      
-                    );
-                    
-                  },
-                );
-              }
-            }
-            )
-            );
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.share),
+                          onPressed: () => Share.share(
+                              "https://web.facebook.com/ekachai.sanharn"),
+                          iconSize: 20,
+                        ),
+                        likeButton(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      '${mypost['subtitle']}',
+                      overflow: TextOverflow.fade,
+                      maxLines: 99,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black),
+                    )),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 15.0,
+                color: Colors.grey.withOpacity(0.4),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Widget likeButton() {
--- lib/three.dart
+++ lib/three.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_ui_designs/appss.dart';
@@ -12,132 +14,189 @@ class ThreeTab extends StatefulWidget {
 }
 
 class _ThreeTab extends State<ThreeTab> {
+  // Field
   List lessons;
+  String uidLogin;
 
+  List<String> nameTeams = List();
+  List<String> urlTeams = List();
+
+  // Method
   @override
   void initState() {
     lessons = getLessons();
     super.initState();
+    findUid();
+  }
+
+  Future<void> findUid() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    uidLogin = firebaseUser.uid;
+    readAllTeam();
+    // readTeam();
+  }
+
+  Future<void> readAllTeam() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Team');
+    await collectionReference.snapshots().listen((QuerySnapshot querySnapshot) {
+      for (var object in querySnapshot.documents) {
+        String nameTeam = object.data['NameTeam'];
+        String urlTeam = object.data['UrlTeam'];
+        
+
+        setState(() {
+          nameTeams.add(nameTeam);
+          urlTeams.add(urlTeam);
+        });
+      }
+    });
+  }
+
+  Future<void> readTeam() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Team');
+    await collectionReference
+        .document(uidLogin)
+        .snapshots()
+        .listen((DocumentSnapshot documentSnapshot) {
+      print('document = ${documentSnapshot.data}');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    ListTile makeListTile(Lesson lesson) => ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-          leading: CircleAvatar(
-            radius: 25.0,
-            backgroundImage: AssetImage("assets/Team.jpg"),
-          ),
-          title: Row(
-            children: <Widget>[
-              Text(
-                lesson.title,
-                overflow: TextOverflow.fade,
-                maxLines: 1,
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600),
-              ),
-            ],
-          ),
-          subtitle: Row(
-            children: <Widget>[
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    width: 20,
-                    child: LinearProgressIndicator(
-                        backgroundColor: Colors.black26,
-                        value: lesson.indicatorValue,
-                        valueColor: AlwaysStoppedAnimation(Colors.green)),
-                  )),
-              SizedBox(
-                width: 10.0,
-              ),
-              Expanded(
-                flex: 4,
-                child: Padding(
-                    padding: EdgeInsets.only(left: 0.0),
-                    child: Text(lesson.address,
-                        overflow: TextOverflow.fade,
-                        maxLines: 1,
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 11,
-                        ))),
-              ),
-            ],
-          ),
-          trailing: Text(lesson.member,
-              overflow: TextOverflow.fade,
-              maxLines: 1,
-              style: TextStyle(color: Colors.black87, fontSize: 12)),
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => TwoTab()));
-          },
-        );
-
-    Card makeCard(Lesson lesson) => Card(
-          elevation: 3.0,
-          semanticContainer: true,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4.0),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
-          child: Container(
-            decoration: BoxDecoration(color: Colors.white12),
-            child: makeListTile(lesson),
-          ),
-        );
-
-    final makeBody = Container(
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: lessons.length,
-        itemBuilder: (BuildContext context, int index) {
-          return makeCard(lessons[index]);
-        },
-      ),
-    );
-
-    final topAppBar = AppBar(
-      leading: IconButton(
-        icon: Icon(Icons.navigate_before),
-        onPressed: () {
-          // Navigator.of(context).pop();
-          MaterialPageRoute materialPageRoute =
-              MaterialPageRoute(builder: (BuildContext buildContext) {
-            return CustomAppBarDemo(currentIndex: 1,);
-          });
-          Navigator.of(context).pushAndRemoveUntil(
-              materialPageRoute, (Route<dynamic> route) => false);
-        },
-      ),
-      elevation: 0.1,
-      backgroundColor: Colors.black54,
-      title: Text('Soccer Community'),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Icon(Icons.list),
-          onPressed: () {},
-        )
-      ],
-    );
-
-    return Scaffold(
-      backgroundColor: Colors.white.withOpacity(0.8),
-      appBar: topAppBar,
-      body: makeBody,
-    );
+    return Scaffold(body: ListView.builder(
+      itemCount: nameTeams.length,
+      itemBuilder: (BuildContext context, int index) {
+        // return Text(nameTeams[index]);
+        return Container(height: 100.0,child: Image.network(urlTeams[index]),);
+      },
+    ),);
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   ListTile makeListTile(Lesson lesson) => ListTile(
+  //         contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+  //         leading: CircleAvatar(
+  //           radius: 25.0,
+  //           backgroundImage: AssetImage("assets/Team.jpg"),
+  //         ),
+  //         title: Row(
+  //           children: <Widget>[
+  //             Text(
+  //               lesson.title,
+  //               overflow: TextOverflow.fade,
+  //               maxLines: 1,
+  //               style: TextStyle(
+  //                   color: Colors.black87,
+  //                   fontSize: 16,
+  //                   fontWeight: FontWeight.w600),
+  //             ),
+  //           ],
+  //         ),
+  //         subtitle: Row(
+  //           children: <Widget>[
+  //             Expanded(
+  //                 flex: 1,
+  //                 child: Container(
+  //                   width: 20,
+  //                   child: LinearProgressIndicator(
+  //                       backgroundColor: Colors.black26,
+  //                       value: lesson.indicatorValue,
+  //                       valueColor: AlwaysStoppedAnimation(Colors.green)),
+  //                 )),
+  //             SizedBox(
+  //               width: 10.0,
+  //             ),
+  //             Expanded(
+  //               flex: 4,
+  //               child: Padding(
+  //                   padding: EdgeInsets.only(left: 0.0),
+  //                   child: Text(lesson.address,
+  //                       overflow: TextOverflow.fade,
+  //                       maxLines: 1,
+  //                       style: TextStyle(
+  //                         color: Colors.black87,
+  //                         fontSize: 11,
+  //                       ))),
+  //             ),
+  //           ],
+  //         ),
+  //         trailing: Text(lesson.member,
+  //             overflow: TextOverflow.fade,
+  //             maxLines: 1,
+  //             style: TextStyle(color: Colors.black87, fontSize: 12)),
+  //         onTap: () {
+  //           Navigator.push(
+  //               context, MaterialPageRoute(builder: (context) => TwoTab()));
+  //         },
+  //       );
+
+  //   Card makeCard(Lesson lesson) => Card(
+  //         elevation: 3.0,
+  //         semanticContainer: true,
+  //         clipBehavior: Clip.antiAliasWithSaveLayer,
+  //         shape: RoundedRectangleBorder(
+  //           borderRadius: BorderRadius.circular(4.0),
+  //         ),
+  //         margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+  //         child: Container(
+  //           decoration: BoxDecoration(color: Colors.white12),
+  //           child: makeListTile(lesson),
+  //         ),
+  //       );
+
+  //   final makeBody = Container(
+  //     child: ListView.builder(
+  //       scrollDirection: Axis.vertical,
+  //       shrinkWrap: true,
+  //       itemCount: lessons.length,
+  //       itemBuilder: (BuildContext context, int index) {
+  //         return makeCard(lessons[index]);
+  //       },
+  //     ),
+  //   );
+
+  //   final topAppBar = AppBar(
+  //     leading: IconButton(
+  //       icon: Icon(Icons.navigate_before),
+  //       onPressed: () {
+  //         // Navigator.of(context).pop();
+  //         MaterialPageRoute materialPageRoute =
+  //             MaterialPageRoute(builder: (BuildContext buildContext) {
+  //           return CustomAppBarDemo(
+  //             currentIndex: 1,
+  //           );
+  //         });
+  //         Navigator.of(context).pushAndRemoveUntil(
+  //             materialPageRoute, (Route<dynamic> route) => false);
+  //       },
+  //     ),
+  //     elevation: 0.1,
+  //     backgroundColor: Colors.black54,
+  //     title: Text('Soccer Community'),
+  //     actions: <Widget>[
+  //       IconButton(
+  //         icon: Icon(Icons.search),
+  //         onPressed: () {},
+  //       ),
+  //       IconButton(
+  //         icon: Icon(Icons.list),
+  //         onPressed: () {},
+  //       )
+  //     ],
+  //   );
+
+  //   return Scaffold(
+  //     backgroundColor: Colors.white.withOpacity(0.8),
+  //     appBar: topAppBar,
+  //     body: makeBody,
+  //   );
+  // }
+
 }
 
 List getLessons() {
